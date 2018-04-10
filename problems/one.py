622e5c263d55b0ee8e2affbfb6983229e3b3539a
CJ SPyNGo <cjspyngo@gmail.com>
2018-04-09 20:53:36 -0400

    rearrange problem one unit tests

--- problems/one.py
+++ problems/one.py
@@ -1,3 +1,5 @@
+import unittest
+
 def sum_from_range(min, max):
     sum = 0
     for x in range(min, max):
@@ -6,3 +8,19 @@ def sum_from_range(min, max):
             print(x)
     print("The sum is: %d" % sum)
     return sum
+
+class TestProblemOne(unittest.TestCase):
+
+    def test_10(self):
+        s = sum_from_range(1, 10)
+        self.assertEqual(s, 23)
+
+    def test_1000(self):
+        s = sum_from_range(1, 1000)
+        self.assertEqual(s, 233168)
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
